a9fbe206617fcd4e738fa2539888de77aff4e897
slash-arkh <slash-arkh@mail.ru>
2024-05-29 18:49:59 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,8 @@
 c = 0
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 while my_list[c] >= 0:
+   if my_list[c] == 0:
+      c += 1
+      continue
    print(my_list[c])
-   c = c + 1
-
+   c += 1
