68499b9f1844ad65e01555068178cf1eec29a96a
slash-arkh <slash-arkh@mail.ru>
2024-05-30 19:17:34 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        a = []
+        for j in range(m):
+            a.append(value)
+        matrix.append(a)
+    return[matrix]
+
+result1 = get_matrix(5,3,15)
+result2 = get_matrix(2,3,4)
+result3 = get_matrix(5,5,5)
+print(result1)
+print(result2)
+print(result3)
